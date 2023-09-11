db680df65d7235eb7e974bf8842790935b0d5214
Peter Chen <peterchen2017125@gmail.com>
2023-09-11 21:46:20 +0800

    20230911

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public  class Variable {
+    public static  void main(String[] args){
+        int money = 100;
+        int age;
+        age = 18;
+        System.out.println(money);
+        System.out.println("我今年"+age);
+    }
+}
