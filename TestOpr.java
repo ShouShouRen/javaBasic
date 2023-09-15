128e99fceaa8a270f984840c416e6ab2f17b5a37
Peter Chen <peterchen2017125@gmail.com>
2023-09-15 08:40:46 +0800

    20230915

--- TestOpr.java
+++ TestOpr.java
@@ -7,6 +7,18 @@ public class TestOpr {
         System.out.println("a-b="+(a-b)); //10
         System.out.println("a*b="+(b*a)); //10
         System.out.println("a/b="+(b/a)); //10
-        System.out.println("a%b="+(a%b)); //10
+        System.out.println("a%b="+(double)(a%b)); //10
+        //System.out.println("5/0="+(5/0));
+        System.out.println("5.0/0="+(5.0/0)); //infinity 無限大
+        System.out.println("5.0/0="+(0.0/0)); //NaN
+        // ++ -- 自增
+        // 如果++在變數前就是先自增在使用
+        // 如果++在變數後就是先使用變數在自增
+
+        //System.out.println(a++);
+        //System.out.println(b++);
+
+        System.out.println(++a);
+        System.out.println(--b);
     }
 }
