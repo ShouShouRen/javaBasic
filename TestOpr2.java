602c468e8a54ad590b4f4d73dcaf2877325f11ed
Peter Chen <peterchen2017125@gmail.com>
2023-09-15 22:27:59 +0800

    20230915

---
+++ TestOpr2.java
@@ -0,0 +1,11 @@
+public class TestOpr2 {
+    public static void main(String[] args){
+        int a = 8, b = 10;
+        int c = a + b;
+        System.out.println("c="+c);
+        int d = 1;
+        d+=a; // d = d + a
+        //System.out.println("d="+d);
+        System.out.println("");
+    }
+}
