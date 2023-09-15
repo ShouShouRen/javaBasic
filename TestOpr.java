f10c63c832cf97e11c9baef4f2a1678fddb0255e
Peter Chen <peterchen2017125@gmail.com>
2023-09-15 08:28:42 +0800

    20230915

---
+++ TestOpr.java
@@ -0,0 +1,12 @@
+public class TestOpr {
+    public static void main(String[] args){
+//        算數運算 + - * / %
+        int a = 10, b = 20;
+        System.out.println("a+b="+a+b); //1020
+        System.out.println("a+b="+(a+b)); //30
+        System.out.println("a-b="+(a-b)); //10
+        System.out.println("a*b="+(b*a)); //10
+        System.out.println("a/b="+(b/a)); //10
+        System.out.println("a%b="+(a%b)); //10
+    }
+}
--- TestType.java
+++ TestType.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class TestType {
     public static  void main(String[] args){
         byte b1 = 100; //-128~127
@@ -10,5 +12,21 @@ public class TestType {
         System.out.println("t1="+t1);
         long l1 = 2147483648L;
         System.out.println("l1="+l1);
+        float f1 = 3.14f;
+        System.out.println("f1="+f1);
+//        float f2 = l1;
+//        System.out.println("f2="+f2);
+        double d1 = 3.1415926535;
+        System.out.println("d1="+d1);
+        char c1 = 'A';
+        System.out.println("cl="+c1);
+        System.out.println("cl to int ="+(int)c1);
+        char c2 = 65;
+        System.out.println("c2="+c2);
+        System.out.println((int)'中');
+        boolean isLogin = true;
+        System.out.println("isLogin="+isLogin);
+        String myName = "陳暐仁";
+        System.out.println("myName="+myName);
     }
 }
