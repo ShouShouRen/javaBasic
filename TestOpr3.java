38b8fbb497044bc99ce2822ddbd87a9984d4eafa
Peter Chen <peterchen2017125@gmail.com>
2023-09-16 15:13:13 +0800

    20230916

--- TestOpr3.java
+++ TestOpr3.java
@@ -1,6 +1,26 @@
 public class TestOpr3 {
     public static void main(String[] args){
+        //int a = 8, b = 10;
+        //System.out.println(a>=b);
+        int chinese = 80, math = 59 , english = 100;
+        //System.out.println(chinese>=60&&math>=60&&english>=60);
+        //System.out.println(chinese>=60||math>=60||english>=60);
+
+        boolean flag = true;
+//        if(flag==true){
+//            flag = false;
+//        }else {
+//            flag=true;
+//        }
+        flag = !flag;
+        //System.out.println("flag="+flag);
         int a = 8, b = 10;
-        System.out.println(a>=b);
+        //System.out.println(a < b && b++ == 10);
+        //System.out.println(b);
+
+        //System.out.println(a > 9 && b++ == 10);
+        //System.out.println(b);
+
+        System.out.println(a > b ? a: b);
     }
 }
